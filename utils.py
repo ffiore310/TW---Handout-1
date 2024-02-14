b60e7f6c5ce72d3a3516be52f6a55189bfa2c4e4
Fernando Fiore <fernandoof@al.insper.edu.br>
2024-02-14 13:35:12 -0300

    part 3

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 def extract_route(request):
     linhas = request.split()
     caminho = linhas[1]
@@ -7,3 +10,15 @@ def extract_route(request):
 def read_file(path):
     byte = open(path, 'rb')
     return byte.read()
+
+def load_data(filename):
+    file_path = os.path.join("data", filename)
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+def load_template(filename):
+    file_path = os.path.join("templates", filename)
+    with open(file_path, 'r') as file:
+        template = file.read()
+    return template
