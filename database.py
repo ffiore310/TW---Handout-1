5d068306deb96c0a6b684bec65a3367ea69c6a4f
Fernando Fiore <fernandoof@al.insper.edu.br>
2024-02-22 08:43:17 -0300

    handout 3 feito

--- database.py
+++ database.py
@@ -12,6 +12,24 @@ class Database():
         self.cur.execute("INSERT INTO note (title, content) VALUES (?, ?)", (note.title, note.content))
         self.conn.commit()
 
+    def get_all(self):
+        l = []
+        cursor = self.cur.execute("SELECT id, title, content FROM note")
+        for linha in cursor:
+            id = linha[0]
+            title = linha[1]
+            content = linha[2]
+            l.append(Note(id, title, content))
+        return l
+    
+    def update(self,entry):
+        self.cur.execute("UPDATE note SET title = ?, content = ? WHERE id = ?", (entry.title, entry.content, entry.id))
+        self.conn.commit()
+
+    def delete(self, note_id):
+        self.cur.execute("DELETE FROM note WHERE id = ?", (note_id,))
+        self.conn.commit()
+
 class Note:
     def __init__(self, id=None, title=None, content=''):
         self.id = id
